4977263a2c0167dbb2412026b868bf7100f9586b
pree-T <miss.preetisarkar0107@gmail.com>
2021-04-05 09:51:18 +0530

    2nd Commit

--- app/src/main/java/com/example/allnews/Adapter.java
+++ app/src/main/java/com/example/allnews/Adapter.java
@@ -1,5 +1,6 @@
 package com.example.allnews;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,7 +15,13 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.allnews.Models.Articles;
 import com.squareup.picasso.Picasso;
 
+import org.ocpsoft.prettytime.PrettyTime;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
     Context context;
@@ -34,15 +41,16 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
     @Override
     public Adapter.ViewHolder onCreateViewHolder(@NonNull @org.jetbrains.annotations.NotNull ViewGroup parent, int viewType) {
         View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.items,parent,false);
-        return  new ViewHolder(view);
+        return new ViewHolder(view);
     }
 
+    @SuppressLint("SetTextI18n")
     @Override
     public void onBindViewHolder(@NonNull @org.jetbrains.annotations.NotNull Adapter.ViewHolder holder, int position) {
         Articles a=articles.get(position);
         holder.tvTitle.setText(a.getTitle());
         holder.tvSource.setText(a.getSource().getName());
-        holder.tvDate.setText(a.getPub());
+        holder.tvDate.setText("\u2022"+dateTime(a.getPub()));
 
         String imageUrl=a.getUtom();
         Picasso.get().load(imageUrl).into(holder.imageView);
@@ -53,7 +61,7 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
     public int getItemCount() {
         return articles.size();
     }
-    public class ViewHolder extends RecyclerView.ViewHolder
+    public static class ViewHolder extends RecyclerView.ViewHolder
     {
         TextView tvTitle,tvSource,tvDate;
         ImageView imageView;
@@ -70,5 +78,29 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
 
 
         }
+    }
+    public String dateTime(String t)
+    {
+        PrettyTime prettyTime=new PrettyTime(new Locale(getCountry()));
+        String time=null;
+        try {
+
+                SimpleDateFormat simpleDateFormat=new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:",Locale.ENGLISH);
+                Date date=simpleDateFormat.parse(t);
+                time=prettyTime.format(date);
+
+        }
+        catch( ParseException e)
+        {
+            e.printStackTrace();
+        }
+        return time;
+
+    }
+    public String getCountry()
+    {
+        Locale locale= Locale.getDefault();
+        String country=locale.getCountry();
+        return country.toLowerCase();
     }
 }
--- app/src/main/java/com/example/allnews/MainActivity.java
+++ app/src/main/java/com/example/allnews/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.allnews;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
 import android.widget.Toast;
@@ -21,6 +22,7 @@ import retrofit2.Response;
 public class MainActivity extends AppCompatActivity {
     final String API_KEY="75b7dfaab177448d89ea53172c3133a1";
     RecyclerView recyclerView;
+    SwipeRefreshLayout swipeRefreshLayout;
     Adapter adapter;
     List<Articles> articles=new ArrayList<>();
 
@@ -28,18 +30,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        swipeRefreshLayout=findViewById(R.id.swipe);
         recyclerView=findViewById(R.id.recyclerv);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         String country=getCountry();
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                retrieveJson(country,API_KEY);
+            }
+        });
+
+
         retrieveJson(country,API_KEY);
     }
     public  void retrieveJson(String country,String apikey){
+        swipeRefreshLayout.setRefreshing(true);
         Call<HeadLines> call=ApiClient.getInstance().getApi().getHeadlines(country, apikey);
         call.enqueue(new Callback<HeadLines>() {
             @Override
             public void onResponse(Call<HeadLines> call, Response<HeadLines> response) {
                 if(response.isSuccessful() && response.body().getArticles()!=null)
                 {
+                    swipeRefreshLayout.setRefreshing(false);
                     articles.clear();
                     articles=response.body().getArticles();
                     adapter=new Adapter(MainActivity.this,articles);
@@ -49,6 +62,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(Call<HeadLines> call, Throwable t) {
+                swipeRefreshLayout.setRefreshing(false);
                 Toast.makeText(MainActivity.this,t.getLocalizedMessage(),Toast.LENGTH_SHORT).show();
 
             }
